e95b82650d91e53014b053460cd65a43f99b95d8
anand-yv <anand93056ad@gmail.com>
2023-04-15 18:38:24 +0530

    Finished home page that is index.html
    In that finished the Courses Section, Frequently Asked Questions(FAQS), Testimonial Section, and Footer Section -15/04/2023 - 14:30-18:40-04

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
+
 // change navbar style on scroll
-window.addEventListener('scroll',() => {
-    document.querySelector('nav').classList.toggle('window-scroll',window.scrollY > 0)
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
+})
+
+
+// Show/Hide FAQ's answer
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        // chnange arrow icon
+        const icon = faq.querySelector('.faq_icon i');
+        if (icon.className === "uil uil-plus"){
+            icon.className = "uil uil-minus";
+        }
+        else{
+            icon.className = 'uil uil-plus';
+        }
+    })
 })

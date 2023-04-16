7563315990c4a592117f4999b435ff1e7feea5bf
anand-yv <anand93056ad@gmail.com>
2023-04-16 18:05:20 +0530

    Made it responsive for Tablet Version
    Done using media queries- it will change for whose screen size is less than 1024px changes is made to whole page in nav bar javascript is used to open and close the button and animation is also done in that from bottom top to top. 15:00-18:04-02-16/04/2023

--- main.js
+++ main.js
@@ -14,11 +14,35 @@ faqs.forEach(faq => {
 
         // chnange arrow icon
         const icon = faq.querySelector('.faq_icon i');
-        if (icon.className === "uil uil-plus"){
+        if (icon.className === "uil uil-plus") {
             icon.className = "uil uil-minus";
         }
-        else{
+        else {
             icon.className = 'uil uil-plus';
         }
     })
 })
+
+
+
+// SHOW/HIDE MOBILE MENU (NAVBAR)
+const menu = document.querySelector(".nav_menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+// show menu
+
+menuBtn.addEventListener("click", () => {
+    menu.style.display = "flex";
+    menuBtn.style.display = "none";
+    closeBtn.style.display = "inline-block";
+})
+
+// hide menu
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener("click", closeNav);
